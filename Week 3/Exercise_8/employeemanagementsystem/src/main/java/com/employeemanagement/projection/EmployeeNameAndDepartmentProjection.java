61ca8d32cf7bc44c69eef3c8e40b5ddcb2e8355a
Priyankapolam2003 <121125194+priyankap-03@users.noreply.github.com>
2024-08-16 17:31:33 +0530

    Add files via upload

---
+++ Week 3/Exercise_8/employeemanagementsystem/src/main/java/com/employeemanagement/projection/EmployeeNameAndDepartmentProjection.java
@@ -0,0 +1,7 @@
+package com.employeemanagement.projection;
+
+public interface EmployeeNameAndDepartmentProjection {
+    String getName();
+    String getEmail();
+    String getDepartmentName();
+}
